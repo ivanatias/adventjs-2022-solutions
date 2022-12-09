0c62293ef82bd4e2c8cdea4cfd45a82d4f516f3a
Ivan Atias <ivan.d.atias@gmail.com>
2022-12-08 22:46:47 -0400

    fix challenge 08 test assertions

--- challenge-08/index.test.js
+++ challenge-08/index.test.js
@@ -7,26 +7,26 @@ describe('challenge #8 tests', () => {
 
   test('checkPart function returns true given "uwu" as argument', () => {
     const part = 'uwu'
-    expect(checkPart(part)).toEqual(true)
+    expect(checkPart(part)).toBe(true)
   })
 
   test('checkPart function returns false given "midu" as argument', () => {
     const part = 'midu'
-    expect(checkPart(part)).toEqual(false)
+    expect(checkPart(part)).toBe(false)
   })
 
   test('checkPart function returns true given "lolol" as argument', () => {
     const part = 'lolol'
-    expect(checkPart(part)).toEqual(true)
+    expect(checkPart(part)).toBe(true)
   })
 
   test('checkPart function returns true given "yolooloy" as argument', () => {
     const part = 'yolooloy'
-    expect(checkPart(part)).toEqual(true)
+    expect(checkPart(part)).toBe(true)
   })
 
   test('checkPart function returns true given "zzzoonzzz" as argument', () => {
     const part = 'zzzoonzzz'
-    expect(checkPart(part)).toEqual(true)
+    expect(checkPart(part)).toBe(true)
   })
 })
