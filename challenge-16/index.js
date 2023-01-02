8e45ac634591cedbf37470df14becff99c1a612b
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-02 19:59:37 -0400

    add challenge 16

---
+++ challenge-16/index.js
@@ -0,0 +1,13 @@
+function fixLetter(letter) {
+  return letter
+    .trim()
+    .replace(/,\s{0,1}/g, ', ')
+    .replace(/\s+/g, ' ')
+    .replace(/\s{1}([,.?!])/g, '$1')
+    .replace(/([a-z])$/gi, '$1.')
+    .replace(/\?+/g, '?')
+    .replace(/santa claus/gi, 'Santa Claus')
+    .replace(/[?!.]+\s[a-z]|^[a-z]/gi, c => c.toUpperCase())
+}
+
+module.exports = fixLetter
