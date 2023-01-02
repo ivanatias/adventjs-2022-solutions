168fa1ff1af874db7116e1a97a8e9fcfff75e04e
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-02 19:33:52 -0400

    add challenge 14

---
+++ challenge-14/index.js
@@ -0,0 +1,9 @@
+function getOptimalPath(path) {
+  return path
+    .reverse()
+    .reduce((acc, curr) =>
+      curr.map((num, i) => num + Math.min(acc[i], acc[i + 1]))
+    )[0]
+}
+
+module.exports = getOptimalPath
