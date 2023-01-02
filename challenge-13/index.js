5d3f72f860c8b8ecba77bf448f493893375af055
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-02 19:20:22 -0400

    add challenge 13

---
+++ challenge-13/index.js
@@ -0,0 +1,9 @@
+function getFilesToBackup(lastBackup, changes) {
+  const set = new Set()
+  const nonBackedUpFiles = changes.filter(change => change[1] > lastBackup)
+  nonBackedUpFiles.forEach(file => set.add(file[0]))
+
+  return [...set].sort((a, b) => a - b)
+}
+
+module.exports = getFilesToBackup
