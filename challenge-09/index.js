4bc038c678edb40f1080d8c8b76db11be8c48f0c
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-02 18:34:15 -0400

    add challenge 09

---
+++ challenge-09/index.js
@@ -0,0 +1,11 @@
+function countTime(leds) {
+  return (
+    leds
+      .join('')
+      .repeat(2)
+      .split('1')
+      .sort((a, b) => b.length - a.length)[0].length * 7
+  )
+}
+
+module.exports = countTime
